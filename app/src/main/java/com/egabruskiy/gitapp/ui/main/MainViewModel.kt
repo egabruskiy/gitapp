df88c100a7ee832dba804891e02614d94b92fe4e
egabruskiy <distort@bk.ru>
2021-06-03 23:25:34 +0300

    c4

--- app/src/main/java/com/egabruskiy/gitapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/egabruskiy/gitapp/ui/main/MainViewModel.kt
@@ -3,5 +3,9 @@ package com.egabruskiy.gitapp.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun n4() {
+
+    }
+
 }
